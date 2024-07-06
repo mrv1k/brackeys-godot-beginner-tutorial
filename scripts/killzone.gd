ae7d11e6014fbefa9714c243fd3cbc930a30ca99
Victor Voronov <victor@voronov.io>
2024-07-05 23:44:31 -0400

    wip death animation

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,25 @@
 extends Area2D
 
-@onready var timer = $Timer
+@onready var timer_after_hit = $TimerAfterHit
+@onready var timer_before_spawn = $TimerBeforeSpawn
 
+func _ready():
+	timer_after_hit.connect("timeout", self._on_timer_after_hit_timeout)
+	timer_before_spawn.connect("timeout", self._on_timer_before_spawn_timeout)
+	
 func _on_body_entered(body):
-	Engine.time_scale = 0.33
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	if body.name == 'Player':
+		timer_after_hit.start(1.0)
+		body.is_dead = true
+		timer_before_spawn.start(1.5)
 
-
-func _on_timer_timeout():
+func _on_timer_after_hit_timeout():
+	# TODO: play is hit
+	Engine.time_scale = 0.8
+	
+func _on_timer_before_spawn_timeout():
+	# TODO: play is dead
+	# FIXME: get body to drop collision
+	#body.get_node("CollisionShape2D").queue_free()
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,11 +6,28 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_dead = false
+var is_done = false
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_timer = $CoyoteTimer
 
+func _ready():
+	animated_sprite_2d.connect("animation_finished", self._on_animation_finished)
+
+func _on_animation_finished():
+	if animated_sprite_2d.animation == "death":
+		is_done = true
+		print("Death animation finished")
+
 func _physics_process(delta):
+	if is_done:
+		return
+
+	if is_dead:
+		animated_sprite_2d.play("death")
+		return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -48,3 +65,4 @@ func _physics_process(delta):
 	
 	if was_on_floor and !is_on_floor() and not Input.is_action_pressed("jump"):
 		coyote_timer.start()
+
