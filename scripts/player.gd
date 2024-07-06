db3674c28c60d70ce1adcaa6d9a0f67cec4f5973
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2024-07-05 21:43:05 -0400

    brackeys tutorial complete

---
+++ scripts/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_timer = $CoyoteTimer
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or !coyote_timer.is_stopped()):
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	var is_idle = direction == 0
+	var is_run_right = direction == 1
+	var is_run_left = direction == -1
+	
+	if is_idle:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		velocity.x = direction * SPEED
+	
+	if !is_on_floor():
+		animated_sprite_2d.play('jump')
+	elif is_idle:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("run")
+		if is_run_right:
+			animated_sprite_2d.flip_h = false
+		elif is_run_left:
+			animated_sprite_2d.flip_h = true
+
+	var was_on_floor = is_on_floor()
+
+	move_and_slide()
+	
+	if was_on_floor and !is_on_floor() and not Input.is_action_pressed("jump"):
+		coyote_timer.start()
