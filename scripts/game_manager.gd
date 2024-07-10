3a5a9a79e5b6c2175ee9a39580de87ae7fa1cd1b
Victor Voronov <victor@voronov.io>
2024-07-10 00:10:44 -0400

    you collected all coins

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,8 +1,23 @@
 extends Node
 
 var score = 0
-@onready var score_label = $ScoreLabel
+var haiku = 0
+var coinsCount = 0
+
+@onready var score_label = %ScoreLabel
+@onready var coins = %Coins
+# @onready var haiku_label = $HaikuLabel
+
+func _ready():
+	coinsCount = coins.get_child_count()
 
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins!" 
+	if score == coinsCount:
+		score_label.text = "You collected all coins!" 
+	else:
+		score_label.text = "You collected " + str(score) + " out of " + str(coinsCount) + " coins!" 
+ 
+func add_haiku():
+	haiku += 1
+
